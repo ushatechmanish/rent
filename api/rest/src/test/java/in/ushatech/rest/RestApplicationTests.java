7fefd2658532a4b1b7a2572a719b36880b347f26
Manish Chauhan <manish.chauhan@ushatech.in>
2024-08-31 11:53:41 +0530

    first commit

---
+++ api/rest/src/test/java/in/ushatech/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ushatech.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
